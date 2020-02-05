81fc5f3e8c4a00c51f7ddb1457a8280fc3e76927
unknown <leolorin9@gmail.com>
2020-02-06 00:00:59 +0100

    Head/Header/Footer/Script découper

---
+++ layouts/head.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <title>Bières en stock</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <link rel="icon" href="public/img/favicon.ico">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css">
+  <link rel="stylesheet" href="public/css/style.css">
+</head>
